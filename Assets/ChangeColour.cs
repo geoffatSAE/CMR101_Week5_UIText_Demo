76bb3232ea1717ef84a7cd5f9168f3f3ce1050c4
Geoffrey Hil <g.hill@sae.edu>
2020-10-28 11:18:29 +1100

    Initial Commit

---
+++ Assets/ChangeColour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColour : MonoBehaviour
+{
+
+    public Text uiTextObject;
+    public TextMesh textMeshObject;
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetRed() 
+    {
+        r.material.color = Color.red;
+
+        uiTextObject.text = "colour is now red";
+        textMeshObject.text = "colour is now red";
+    }
+
+    public void SetBlue()
+    {
+        r.material.color = Color.blue;
+
+        uiTextObject.text = "colour is now blue";
+        textMeshObject.text = "colour is now blue";
+    }
+}
